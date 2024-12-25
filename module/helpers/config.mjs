eb5baa64fe5ed347128c70a476bd8df5f32be0e2
Miles Eventide <dalton.sterritt@gmail.com>
2024-12-24 20:59:55 -0700

    Added "hidden abilities" that are tracked on the character sheets for roll purposes

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -19,3 +19,25 @@ EVENTIDE_RP_SYSTEM.abilityAbbreviations = {
   will: 'EVENTIDE_RP_SYSTEM.Ability.Will.abbr',
   wits: 'EVENTIDE_RP_SYSTEM.Ability.Wits.abbr',
 };
+
+EVENTIDE_RP_SYSTEM.hiddenAbilities = {
+  dice: `EVENTIDE_RP_SYSTEM.Ability.Dice.long`,
+  cmax: `EVENTIDE_RP_SYSTEM.Ability.Cmax.long`,
+  cmin: `EVENTIDE_RP_SYSTEM.Ability.Cmin.long`,
+  acrodc: `EVENTIDE_RP_SYSTEM.Ability.AcroDC.long`,
+  physdc: `EVENTIDE_RP_SYSTEM.Ability.PhysDC.long`,
+  fortdc: `EVENTIDE_RP_SYSTEM.Ability.FortDC.long`,
+  willdc: `EVENTIDE_RP_SYSTEM.Ability.WillDC.long`,
+  witsdc: `EVENTIDE_RP_SYSTEM.Ability.WitsDC.long`,
+};
+
+EVENTIDE_RP_SYSTEM.hiddenAbilityAbbreviations = {
+  dice: `EVENTIDE_RP_SYSTEM.Ability.Dice.abbr`,
+  cmax: `EVENTIDE_RP_SYSTEM.Ability.Cmax.abbr`,
+  cmin: `EVENTIDE_RP_SYSTEM.Ability.Cmin.abbr`,
+  acrodc: `EVENTIDE_RP_SYSTEM.Ability.AcroDC.abbr`,
+  physdc: `EVENTIDE_RP_SYSTEM.Ability.PhysDC.abbr`,
+  fortdc: `EVENTIDE_RP_SYSTEM.Ability.FortDC.abbr`,
+  willdc: `EVENTIDE_RP_SYSTEM.Ability.WillDC.abbr`,
+  witsdc: `EVENTIDE_RP_SYSTEM.Ability.WitsDC.abbr`,
+};
