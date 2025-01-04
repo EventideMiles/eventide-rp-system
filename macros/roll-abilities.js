6ca8ba1d0273f1d61e004fc989fdca54a6042d82
Miles Eventide <dalton.sterritt@gmail.com>
2025-01-03 18:50:19 -0700

    Updated some of the pre-built macros.

--- macros/roll-abilities.js
+++ macros/roll-abilities.js
@@ -1,7 +1,51 @@
-const tokenArray = await game.erps.getSelectedArray();
+const Acrobatics = async () => {
+  const tokenArray = await game.erps.getSelectedArray();
 
-if (!tokenArray.length) return ui.notifications.error("Please select a token.");
+  if (!tokenArray.length)
+    return ui.notifications.error("Please select a token.");
 
-for (const token of tokenArray) {
-  token.actor.rollAbility({ ability: "acro" });
-}
+  for (const token of tokenArray) {
+    token.actor.rollAbility({ ability: "acro" });
+  }
+};
+
+const Physical = async () => {
+  const tokenArray = await game.erps.getSelectedArray();
+
+  if (!tokenArray.length)
+    return ui.notifications.error("Please select a token.");
+
+  for (const token of tokenArray) {
+    token.actor.rollAbility({ ability: "phys" });
+  }
+};
+const Fortitude = async () => {
+  const tokenArray = await game.erps.getSelectedArray();
+
+  if (!tokenArray.length)
+    return ui.notifications.error("Please select a token.");
+
+  for (const token of tokenArray) {
+    token.actor.rollAbility({ ability: "fort" });
+  }
+};
+const Will = async () => {
+  const tokenArray = await game.erps.getSelectedArray();
+
+  if (!tokenArray.length)
+    return ui.notifications.error("Please select a token.");
+
+  for (const token of tokenArray) {
+    token.actor.rollAbility({ ability: "will" });
+  }
+};
+const Wits = async () => {
+  const tokenArray = await game.erps.getSelectedArray();
+
+  if (!tokenArray.length)
+    return ui.notifications.error("Please select a token.");
+
+  for (const token of tokenArray) {
+    token.actor.rollAbility({ ability: "wits" });
+  }
+};
