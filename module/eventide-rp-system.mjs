56b7128780313fd5ca2e62912995af98076af624
Dalton Sterritt <dalton.sterritt@gmail.com>
2025-01-13 03:57:09 -0700

    Added a useful handlebars helper to assist with debugging.

--- module/eventide-rp-system.mjs
+++ module/eventide-rp-system.mjs
@@ -138,6 +138,17 @@ Handlebars.registerHelper("console", function (str) {
   console.log(str);
 });
 
+Handlebars.registerHelper("debug", function (optionalValue) {
+  console.log("Current Context");
+  console.log("====================");
+  console.log(this);
+  if (optionalValue) {
+    console.log("Value");
+    console.log("====================");
+    console.log(optionalValue);
+  }
+});
+
 /* -------------------------------------------- */
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
