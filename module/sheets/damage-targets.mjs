a334729508128b5572ee63a1616ee690fd2f72b5
Dalton Sterritt <dalton.sterritt@gmail.com>
2025-02-04 03:43:46 -0700

    Put in handling to ensure script will default to targets if no selected are available.

--- module/sheets/damage-targets.mjs
+++ module/sheets/damage-targets.mjs
@@ -124,7 +124,7 @@ export class DamageTargets extends HandlebarsApplicationMixin(ApplicationV2) {
       description: form.description.value || "",
       type: form.isHeal.checked ? "heal" : "damage",
     };
-    if (damageOptions.type === "heal") {
+    if (damageOptions.type === "heal" && selectedArray.length > 0) {
       await Promise.all(
         this.selectedArray.map((token) =>
           token.actor.damageResolve(damageOptions)
