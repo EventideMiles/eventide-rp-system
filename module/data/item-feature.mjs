c2c67caa34d6ee2ee0ead40079854fa683e6b99c
Dalton Sterritt <dalton.sterritt@gmail.com>
2025-02-20 03:01:44 -0700

    Updated schema implementation to include the same structure as status effects for features: they're really two sides of the same coin.

--- module/data/item-feature.mjs
+++ module/data/item-feature.mjs
@@ -1,3 +1,21 @@
 import EventideRpSystemItemBase from "./base-item.mjs";
 
-export default class EventideRpSystemFeature extends EventideRpSystemItemBase {}
+export default class EventideRpSystemFeature extends EventideRpSystemItemBase {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = super.defineSchema();
+
+    schema.bgColor = new fields.ColorField({
+      initial: "#70B87A",
+      blank: false,
+      required: true,
+    });
+    schema.textColor = new fields.ColorField({
+      initial: "#ffffff",
+      blank: false,
+      required: true,
+    });
+
+    return schema;
+  }
+}
